fb56ad031f04ba52d9ee3eae3d057c48a38aa31d
Aleksey Shvedchikov <a1exs@yandex-team.ru>
2022-06-19 21:16:17 +0300

    Generate pigeon

--- pigeon/pigeon.sh
+++ pigeon/pigeon.sh
@@ -1,5 +1,5 @@
 flutter pub run pigeon \
   --input pigeon/pigeon_inno_plugin.dart \
   --dart_out lib/pigeon/pigeon.dart \
-  --java_out ./android/app/src/main/java/dev/flutter/pigeon/Pigeon.java \
+  --java_out ./android/src/main/kotlin/com/example/inno_plugin/Pigeon.java \
   --java_package "com.example.inno_plugin"
